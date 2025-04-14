83b19037ad61cb9ed0eb4ba9b35e00cb5d273111
Marcos <marcos.chiu@gmail.com>
2025-04-14 17:30:43 -0300

    renamed nginx template

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-envsubst '$DOMAIN_NAME' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$DOMAIN_NAME' < /etc/nginx/nginx-template.conf > /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
