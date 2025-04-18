01d7c277e265317f7a3745f30dc49693743a2be7
Marcos <marcos.chiu@gmail.com>
2025-04-18 12:22:15 -0300

    different setup

--- srcs/requirements/mariadb/tools/db-setup.sh
+++ srcs/requirements/mariadb/tools/db-setup.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+set -e
 
-mariadbd-safe --datadir=/var/lib/mysql &
+echo "DB: $WP_DATABASE_NAME, User: $WP_DATABASE_USER"
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Initializing MariaDB data dir..."
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+fi
+
+# Start MariaDB in the background
+mysqld_safe &
 sleep 5
 
-echo "DB_NAME=$WP_DATABASE_NAME"
-echo "DB_USER=$WP_DATABASE_USER"
-echo "DB_PASSWORD=$WP_DATABASE_PASSWORD"
-
+echo "Creating database and users..."
 mariadb -u root <<EOF
-DROP USER IF EXISTS '${WP_DATABASE_USER}'@'%';
-DROP USER IF EXISTS '${WP_DATABASE_ROOT}'@'localhost';
 DROP USER IF EXISTS ''@'localhost';
 DROP DATABASE IF EXISTS test;
 CREATE DATABASE IF NOT EXISTS \`${WP_DATABASE_NAME}\`;
-CREATE USER '${WP_DATABASE_USER}'@'%' IDENTIFIED BY '${WP_DATABASE_PASSWORD}';
+CREATE USER IF NOT EXISTS '${WP_DATABASE_USER}'@'%' IDENTIFIED BY '${WP_DATABASE_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${WP_DATABASE_NAME}\`.* TO '${WP_DATABASE_USER}'@'%';
-CREATE USER '${WP_DATABASE_ROOT}'@'localhost' IDENTIFIED BY '${WP_DATABASE_ROOT_PASSWORD}';
-ALTER USER '${WP_DATABASE_ROOT}'@'localhost' IDENTIFIED BY '${WP_DATABASE_ROOT_PASSWORD}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${WP_DATABASE_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
 
-mariadb-admin -u root -p${WP_DATABASE_ROOT_PASSWORD} shutdown
+# Stop the temporary MariaDB
+mysqladmin -u root -p${WP_DATABASE_ROOT_PASSWORD} shutdown
+
+# Replace the running process with actual server
+exec mariadbd
