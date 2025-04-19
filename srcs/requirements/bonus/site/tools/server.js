4131f6dfee572bb2df92afa0abeb301bf1fb5c4c
Marcos <marcos.chiu@gmail.com>
2025-04-19 20:00:53 -0300

    f

--- srcs/requirements/bonus/site/tools/server.js
+++ srcs/requirements/bonus/site/tools/server.js
@@ -15,9 +15,16 @@ if (fs.readdirSync(root).length === 0) {
 
 http.createServer((req, res) => {
   if (req.url.startsWith('/ftp/')) {
-    // Redirect to FTPS container
-    res.writeHead(301, { 'Location': 'ftp://ftp:21' });  // Or another FTPS URL
-    res.end();
+    const filePath = path.join('/var/www/site', req.url);
+    fs.readFile(filePath, (err, content) => {
+      if (err) {
+        res.writeHead(404);
+        res.end('404 Not Found');
+      } else {
+        res.writeHead(200);
+        res.end(content);
+      }
+    });
   } else {
     const filePath = path.join(root, req.url === '/' ? '/index.html' : req.url);
     fs.readFile(filePath, (err, content) => {
@@ -33,3 +40,4 @@ http.createServer((req, res) => {
 }).listen(port, () => {
   console.log(`Site served at http://localhost:${port}`);
 });
+
